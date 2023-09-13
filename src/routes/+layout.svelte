b10d4e0aee0a253eeda4f0384585f951f062176a
nickmura <nickmuradev@gmail.com>
2023-09-12 22:14:25 -0600

    created prototype landing site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export const ssr = false;
+	export const prerender = true;
+	export const trailingSlash = 'always';
+	import '../app.css';
+
+	
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+</script>
+
+<main class="flex flex-col p-10 w-screen h-screen pt-40 items-center gap-5 font-serif">
+	<div class=''>
+		<img src='./sentryfinance.png' class='' alt='SentryFi'>
+
+		
+	</div>
+	<div class='pt-2 w-[499px] text-center'>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Sed velit ante, rhoncus id lobortis vel, feugiat vitae nibh. 
+		In venenatis rutrum sem et hendrerit. 
+	</div>
+
+	<div class='p'>
+		<button class='bg-[#1D0991] hover:bg-[#270CC0] hover:scale-[1.10] transition px-2 py-2 rounded-xl text-white font-serif'>Contact us</button> 
+	</div>
+</main>
