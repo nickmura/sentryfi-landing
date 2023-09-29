4a831fa9a6ba4d7525c02cb558e2243ec83b5e1b
nickmura <nickmuradev@gmail.com>
2023-09-28 23:44:20 -0600

    fixed header on mobile, fixed contact

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,12 @@
 		<h1 class='sr-only'>SentryFi SentryFinance</h1>
 		
 	</div>
-	<div class='pt-2 w-[499px] text-center'>
+	<div class='pt-2 max-w-[499px] w-fit text-center px-3'>
 		We specialize in developing cryptocurrency custody and security solutions to institutions and enterprises. We also offer
 		consultancy in our domain. 
 	</div>
 
 	<div class='p'>
-		<button class='bg-[#1D0991] hover:bg-[#270CC0] hover:scale-[1.10] transition px-2 py-2 rounded-xl text-white font-serif'>Contact us</button> 
+		<a href='mailto:info@sentryfi.com' class='bg-[#1D0991] hover:bg-[#270CC0] hover:scale-[1.10] transition px-2 py-2 rounded-xl text-white font-serif'>Contact us</a> 
 	</div>
 </main>
